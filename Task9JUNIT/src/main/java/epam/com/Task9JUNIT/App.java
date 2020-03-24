78df262f06652d768a0ef5a6e39d5534a05ade13
My954 <My954@DESKSMA>
2020-03-24 10:21:40 +0530

    Task Tdd and junit Completed

---
+++ Task9JUNIT/src/main/java/epam/com/Task9JUNIT/App.java
@@ -0,0 +1,13 @@
+package epam.com.Task9JUNIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Task9JUNIT/src/main/java/epam/com/Task9JUNIT/RemoveA.java
@@ -0,0 +1,36 @@
+package epam.com.Task9JUNIT;
+
+public class RemoveA {
+	public String remove(String s)
+	{
+		String result=s;
+		int length=s.length();
+		if(length>2) 
+		{
+			if(s.charAt(0)=='A'&&s.charAt(1)=='A')
+				result=s.substring(2);
+			else if(s.charAt(0)=='A'&&s.charAt(1)!='A')
+				result=s.substring(1);
+			else if(s.charAt(0)!='A'&&s.charAt(1)=='A')
+				result=s.charAt(0)+s.substring(2);
+		}
+		else 
+		{
+			if(length==1)
+			{	
+				if(s.charAt(0)=='A')
+					result="";
+			}
+			else if(length==2)
+			{
+			    if(s.charAt(0)=='A'&&s.charAt(1)=='A')
+					result="";
+				else if(s.charAt(0)=='A'&&s.charAt(1)!='A')
+					result=s.charAt(1)+"";
+				else if(s.charAt(0)!='A'&&s.charAt(1)=='A')
+					result=s.charAt(0)+"";
+			}
+		}
+		return result;
+	}
+}
